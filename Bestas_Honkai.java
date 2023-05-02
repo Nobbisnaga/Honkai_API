4d679fe98bcf1cf63f6f97559dc7246a93ff51ec
Nobbisnaga <99812675+Nobbisnaga@users.noreply.github.com>
2023-05-02 08:58:47 -0300

    Update Bestas_Honkai.java

--- Bestas_Honkai.java
+++ Bestas_Honkai.java
@@ -1,5 +1,12 @@
 package model;
 
 public class Bestas_Honkai {
-    
+    public static void main(String[] args) throws Exception{
+        System.out.println("\n Parvati ; Classe: Imperador ; Manipula Gelo e Neve ; Primeira aparição: Christmas Eve, 1973. \n");
+        System.out.println("\n Ganesha ; Classe: Imperador ; Armadura massiva e grande capacidade de dano de impacto ; Primeira aparição: India, 2012. \n");
+        System.out.println("\n Benares ; Classe: Juiz ; Criatura alada com armadura massiva adaptativa a qualquer elemento ; Existem registros da sua primeira aparição na antiga era junto da segunda Herrscher Sirin, mas nada confirmado, já sua reaparição ocorreu em 2016 na era atual em Nagazora no japão. ");
+        System.out.println("\n Tlaloc ; Classe: Misterio ; Criatura emergida no Mar Quanta após o Olho Profundo conectar-se com o Mar Quanta. Atualmente morto. \n");
+        System.out.println("\n Husk - Nihilius ; Classe: Misterio ; Criatura emergida do portal Imaginario ativado em Nagazora, Japão, 2017. é capaz de manipular o tempo. \n");
+        System.out.println("\n Tonatiuh ; Classe: Vida da Ciência ; Criatura feita em Heliopolis Life Science Campus. Atualmente morto no Mar Quanta. \n");
+    }
 }
