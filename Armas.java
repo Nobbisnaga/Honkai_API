1364ef29d38327f8a1ca05937eb18b080c42e357
Nobbisnaga <99812675+Nobbisnaga@users.noreply.github.com>
2023-05-02 08:57:42 -0300

    Update Armas.java

--- Armas.java
+++ Armas.java
@@ -2,6 +2,16 @@ package model;
 
 public class Armas {
     public static void main(String[] args) throws Exception{
-        System.out.println("\n Oath of Judas \n");
+        System.out.println("\n Tipo: Lança ; Abyss Flower ; Chave Divina feita apartir do nucleo do Herrscher da Morte, o sexto Herrscher da Antiga Era. \n");
+        System.out.println("\n Tipo: Manopla ; Fenhuang Down ; Chave Divina feita apartir do nucleo do oitavo Herrscher da antiga era, o Herrscher da Senciência. \n");
+        System.out.println("\n Tipo: Pistola ; Judgment of Shamash ; Chave Divina feita apartir do nucleo do sétimo Herrscher da antiga era, o Herrscher das chamas. \n");
+        System.out.println("\n Tipo: Bazuca ; Star of Eden ; Chave Divina feita apartir do nono nucleo Herrscher do Vazio, catalogado somente na nova era como Chave Divina por Einstein e Tesla devido a seu poder de destruição e controle da gravidade. \n");
+        System.out.println("\n Tipo: Bazuca ; Quinta Reliquia Sagrada ; Chave divina falha feita de um nucleo Herrscher desconhecido na antiga era pela Schicksal. \n");
+        System.out.println("\n Tipo: Espada ; Terceira Reliquia sagrada ; uma Chave divina falha feita de um nucleo Herrscher desconhecido na antiga era pela Schicksal. \n");
+        System.out.println("\n Tipo: Espadão ; Chave Divina criada na antiga era apartir do nucleo do Herrscher das chamas, não possui numeração, pois seus dados foram perdidos no Mar Quanta em posse de Kevin Kaslana. É conhecida como a Chave da Destruição, uma verdadeira destruidora de Herrschers. \n");
+        System.out.println("\n Tipo: Cruz ; Oath of Shamash ; não é uma Chave divina mas tem o mesmo nivel de poder para enfrentar o Honkai, até o momento é a unica arma conhecida capaz de anular até mesmo a energia Honkai de um Herrscher completamente. \n");
+        System.out.println("\n Tipo: Foice ; Undine's Tale ; Gerada por Seele apartir da ativação da Stigma original do Herrscher da morte, representa sua vontade de protager quem ama acima de tudo. \n");
+        System.out.println("\n Tipo: Arco ; Domain of Ego: Flawless Return ; Essa Arma foi gerada pela Herrscher of Human no Elysian Realm, seus efeitos só tem total proveito nas mãos de Elysian, sendo uma arma capaz de curar a impureza enraizada no coração de qualquer ser vivo incluindo outros Herrschers, com esta arma Elysian foi capaz de derrotar o Herrscher da Corrupção. \n");
+        System.out.println("\n Tipo: Chakram ; Lost Conviction ; Arma assinatura de Aphonia, uma dos 12 Flame Chaser que lutou pela humanidade contra o Honkai na antiga era. \n");
     }
 }
