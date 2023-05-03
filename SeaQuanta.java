a7cb62d405194244cac5e75ac3202f7890eb5b01
Nobbisnaga <99812675+Nobbisnaga@users.noreply.github.com>
2023-05-03 02:39:11 -0300

    Add files via upload

---
+++ SeaQuanta.java
@@ -0,0 +1,9 @@
+package model;
+
+public class SeaQuanta {
+    public static void main (String[]args){
+        System.out.println("\n O Mar Quanta, a Arvore Quanta são como ing Yang, completos opostos que se complementam e não existem um sem o outro, pois a Arvore com cada folha dá a vida a infinitos universos enquanto o Mar apaga e consome a vida, em um ciclo sem fim, o mar apaga todas as folhas/universos que caem nele, consome e converte em energia para a arvore produzir novos universos que ao morrerem, como folhas caindo se desprendem do galho e caem no mar, assim fadando todo universo a nascer e morrer, cada um com seu tempo e tamanho. \n");
+        System.out.println("\n Universo Bolha \n Um Universo Bolha existe quando uma folha flutua dentro de uma bolha antes de cair no Mar, como qualquer universo os universos bolha chegarão ao seu fim, mas irão demorar muito para tal, porem são minusculos em comparação aos universos comuns geralmente. \n");
+    }
+    
+}
